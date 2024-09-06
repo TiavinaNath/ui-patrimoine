e87e74385c1c292e65215d1e39ea6f0f707fe32e
TiavinaNath <hei.tiavina.3@gmail.com>
2024-09-06 19:56:36 +0300

    fix: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Écouter sur toutes les interfaces réseau
-    port: process.env.PORT || 3000, // Utiliser le port fourni par l'environnement ou fallback sur 3000
+    port: process.env.PORT || 5173, // Utiliser le port fourni par l'environnement ou fallback sur 3000
     proxy: {
       '/api': 'http://localhost:3000', // Proxy API requests to the backend server (en local)
     },
